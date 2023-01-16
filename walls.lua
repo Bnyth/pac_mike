8361c236cef98b3e972d7353ce9bef2df713c7c6
bnyth <lfnnery@protonmail.com>
2023-01-16 18:07:57 -0300

    Add functions to check if a certain tile contains a wall
    Co-authored-by: salatine <salatinee@users.noreply.github.com>

--- walls.lua
+++ walls.lua
@@ -1,12 +1,27 @@
 walls = {}
 
 function walls:load()
-    self.walls = {}
+    self.walls = {} 
     if gameMap.layers["Walls"] then
-        for i, object in pairs(gameMap.layers["Walls"].objects) do
+        for _, object in pairs(gameMap.layers["Walls"].objects) do 
             local wall = world:newRectangleCollider(object.x, object.y, object.width, object.height)
-            wall:setType('static')
-            table.insert(self.walls, wall)
+            wall.x, wall.y = object.x, object.y
+            wall.width, wall.height = object.width, object.height
+            wall:setType('static') 
+            table.insert(self.walls, wall) 
+        end 
+    end
+end
+
+function walls:getWalls()
+    return self.walls
+end
+
+function walls:isTileWall(tile)
+    for _, wall in pairs(self.walls) do
+        if checkCollision(wall, tile) == true then
+            return true
         end
     end
+    return false
 end
