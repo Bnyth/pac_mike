675f39b41c3631ef693376dbc87b6afc2af917b6
bnyth <lfnnery@protonmail.com>
2023-01-16 18:10:21 -0300

    Change version to 11.3 and set window.height to match the window.width
    Co-authored-by: salatine <salatinee@users.noreply.github.com>

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.title = "Pac Mike"
-    t.version = "11.4"
+    t.version = "11.3"
     t.console = true
     t.window.width = 672
-    t.window.height = 672
+    t.window.height = t.window.width
 end
