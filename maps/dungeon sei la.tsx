97d44abca0f9b2b756e1d9d0bb67032d46f016dc
Bnyth <lfnnery@protonmail.com>
2023-02-09 19:24:41 -0300

    Add a new customized map and new tilesets
    Co-authored-by: salatine <salatinee@users.noreply.github.com>

---
+++ maps/dungeon sei la.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="dungeon sei la" tilewidth="36" tileheight="36" tilecount="24" columns="6">
+ <image source="../assets/dungeonTileset.png" width="216" height="144"/>
+</tileset>
