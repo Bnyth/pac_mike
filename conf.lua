5e5454ca4908e4a03394aac1e7e6f08f81514a3d
Bnyth <lfnnery@protonmail.com>
2023-02-09 19:23:41 -0300

    Change window size and add the newMap.lua in the necessary fields
    Co-authored-by: salatine <salatinee@users.noreply.github.com>

--- conf.lua
+++ conf.lua
@@ -2,6 +2,6 @@ function love.conf(t)
     t.title = "Pac Mike"
     t.version = "11.3"
     t.console = true
-    t.window.width = 672
+    t.window.width = 756
     t.window.height = t.window.width
 end
